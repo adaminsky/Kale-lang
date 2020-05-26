e01b1f3bd0b6b011bd7af259990966c4c0f9bab6
Adam Stein <adaminsky@gmail.com>
2020-05-25 19:54:38 -0700

    Refactor of basic code layout to use header files

---
+++ src/lexer.h
@@ -0,0 +1,22 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+// The lexer returns tokens [0-255] if it is an unknown character, otherwise one
+// of these for known things.
+enum Token {
+  tok_eof = -1,
+
+  // commands
+  tok_def = -2,
+  tok_extern = -3,
+
+  // primary
+  tok_identifier = -4,
+  tok_number = -5
+};
+
+static std::string IdentifierStr; // Filled in if tok_identifier
+static double NumVal;             // Filled in if tok_number
+
+int gettok();
+#endif	// LEXER_H
